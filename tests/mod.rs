8fad7cc7d96608f8a1c7bd9e59e538faf1f57f6d
thinkier <26728734+thinkier@users.noreply.github.com>
2022-09-13 16:35:18 +1000

    Create mod.rs

---
+++ tests/mod.rs
@@ -0,0 +1,14 @@
+use crate::imp::diff::render_diff;
+
+const DIFF: &'static str = include_str!("wktest.diff");
+const NEW: &'static str = include_str!("wktest.new");
+const ORIGINAL: &'static str = include_str!("wktest.original");
+
+#[test]
+fn test_diff_u_from_wikipedia() {
+    assert_eq!(DIFF, render_diff(&diff::lines(ORIGINAL, NEW))
+        .into_iter()
+        .map(|d| d.to_string())
+        .collect::<Vec<String>>()
+        .join("\n"))
+}
